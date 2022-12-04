f7eb975b0a87b3e4ceaeabdf0f0051493f889e76
tvaccari34 <tiago.vaccari@outlook.com>
2022-12-04 16:13:20 +0000

    First Commit

---
+++ sketch-02.js
@@ -0,0 +1,42 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const degToRad = (degrees) => {
+  return degrees / 180 * Math.PI;
+}
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+
+    const x = width * 0.5;
+    const y = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
+
+    const num = 12;
+
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360) / num;
+      const angle = slice * i;
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(angle);
+  
+      context.beginPath();
+      context.rect( -w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+      
+    }
+  };
+};
+
+canvasSketch(sketch, settings);

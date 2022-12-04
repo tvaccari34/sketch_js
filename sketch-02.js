627db96473714c98e16fc0804615803ccfb85853
Tiago Vaccari <tiago.vaccari@outlook.com>
2022-12-04 17:10:35 +0000

    add utils library

--- sketch-02.js
+++ sketch-02.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -8,6 +10,10 @@ const degToRad = (degrees) => {
   return degrees / 180 * Math.PI;
 }
 
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -15,20 +21,26 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
     const w = width * 0.01;
     const h = height * 0.1;
+    let x, y;
 
     const num = 12;
+    const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360) / num;
+      const slice = math.degToRad(360) / num;
       const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
 
       context.save();
       context.translate(x, y);
-      context.rotate(angle);
+      context.rotate(-angle);
+      context.scale(random.range(1, 3) ,1);
   
       context.beginPath();
       context.rect( -w * 0.5, -h * 0.5, w, h);
